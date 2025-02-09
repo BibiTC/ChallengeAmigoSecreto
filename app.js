96c0b8329e724663a45ffbd4752c0e4ccf36bad1
BibiTC <bibiana.trujillo.bt@gmail.com>
2025-02-09 18:09:34 -0500

    Creación función listaAmigos , para capturar los nombres de los amigos ingresados por pantalla al campo texto id = amigo

--- app.js
+++ app.js
@@ -6,3 +6,20 @@
 ONE Oracle Next Education y Alura
 | Challenge Amigo Secreto
 ==========================================================================================================*/
+
+let listaAmigos = [];           // Lista de amigos
+
+// 1. Función que recibe un array de nombres y lista los nombres ingresados por pantalla.
+function agregarAmigo() {  //Funcion Adicionar Amigo Secreto a la lista
+    if (document.getElementById('amigo').value != '') { //Valda que no sea un valor vacio
+        listaAmigosCompleta.push(document.getElementById('amigo').value);
+        document.getElementById('amigo').value = '';
+        document.getElementById('amigo').focus();
+        console.log(listaAmigosCompleta);
+    } else if (document.getElementById('amigo').value == '') { //Valda si añade un valor vacio
+        alert('Por favor, ingresar un Nombre');
+        return;
+    }
+}
+
+
