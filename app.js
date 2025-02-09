9534e48f3a87b2f251481484fab840e500f3447d
BibiTC <bibiana.trujillo.bt@gmail.com>
2025-02-09 18:13:43 -0500

    Actualiza nombre de variable en el Push estaba con el nombre anterior

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let listaAmigos = [];           // Lista de amigos
 // 1. Función que recibe un array de nombres y lista los nombres ingresados por pantalla.
 function agregarAmigo() {  //Funcion Adicionar Amigo Secreto a la lista
     if (document.getElementById('amigo').value != '') { //Valda que no sea un valor vacio
-        listaAmigosCompleta.push(document.getElementById('amigo').value);
+        listaAmigos.push(document.getElementById('amigo').value);
         document.getElementById('amigo').value = '';
         document.getElementById('amigo').focus();
         console.log(listaAmigos);
