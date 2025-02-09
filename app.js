2b9b8001ed4b78d76f78497335580fc394dcf289
BibiTC <bibiana.trujillo.bt@gmail.com>
2025-02-09 17:55:23 -0500

    Creación Repositorio en Git Hub y en app.js adición Titulo FUncionalidad

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+/*==========================================================================================================
+| Lógica de programación: Práctica
+ONE Oracle Next Education y Alura
+| Challenge Amigo Secreto
+==========================================================================================================*/
