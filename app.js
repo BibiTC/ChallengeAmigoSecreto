57ab95fb093eb976923e6bab6896ca26af99f2db
BibiTC <bibiana.trujillo.bt@gmail.com>
2025-02-09 18:33:45 -0500

    Adicion validacion función agregraAmigo, si el nombre ya esta en la lista genera mensaje para que modifiquen el nombre

--- app.js
+++ app.js
@@ -11,12 +11,21 @@ let listaAmigos = [];           // Lista de amigos
 
 // 1. Función que recibe un array de nombres y lista los nombres ingresados por pantalla.
 function agregarAmigo() {  //Funcion Adicionar Amigo Secreto a la lista
-    if (document.getElementById('amigo').value != '') { //Valda que no sea un valor vacio
-        listaAmigos.push(document.getElementById('amigo').value);
-        document.getElementById('amigo').value = '';
-        document.getElementById('amigo').focus();
-        console.log(listaAmigos);
-    } else if (document.getElementById('amigo').value == '') { //Valda si añade un valor vacio
+    let amigo = document.getElementById('amigo').value; //Obtiene el valor del input
+
+    if (amigo != '') { //Valida que no sea un valor vacio
+        if (listaAmigos.length > 0 && listaAmigos.include(amigo) == false) { //Valda si el amigo ya fue ingresado a la lista
+            listaAmigos.push(document.getElementById('amigo').value);
+            document.getElementById('amigo').value = '';
+            document.getElementById('amigo').focus();
+            console.log(listaAmigos);
+        }else if (listaAmigos.length > 0 && listaAmigos.include(amigo) == true) { //Valda si el amigo ya fue ingresado a la lista
+            alert(`Amigo(a) ${amigo} ya fue agregado(a)`);
+            document.getElementById('amigo').value = '';
+            document.getElementById('amigo').focus();
+
+        }
+    } else if (document.getElementById('amigo').value == '') { //Ingresar valor diferente a vacio
         alert('Por favor, ingresar un Nombre');
         return;
     }
